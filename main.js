7ce62de64daad7027004af1a0dc87bc1daad8467
prasadikduaclk <115408126+prasadikduaclk@users.noreply.github.com>
2022-10-09 19:06:56 +0530

    1 file modified

--- main.js
+++ main.js
@@ -23,8 +23,8 @@ document.body.appendChild (renderer.domElement)
 const sphere = new THREE.Mesh(
   new THREE.SphereGeometry(5, 50, 50),
   new THREE.MeshBasicMaterial({
-    //color: 0xff0000
-    map : new THREE.TextureLoader().load("https://b3d.interplanety.org/wp-content/upload_content/2016/08/01-3.jpg")
+    color: 0xff0000
+    //map : new THREE.TextureLoader().load()
   })
 )
 
