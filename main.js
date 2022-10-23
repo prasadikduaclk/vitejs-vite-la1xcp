7fa6ed2a04477a57cddea9c7a18309f85d9822b3
prasadikduaclk <115408126+prasadikduaclk@users.noreply.github.com>
2022-10-23 15:30:56 +0530

    3 files modified, 3 added

--- main.js
+++ main.js
@@ -3,6 +3,9 @@
 //import { setupCounter } from './counter.js'
 
 import * as THREE from "https://cdn.skypack.dev/three@0.145.0"
+import vertexShader from '/vertex.glsl'
+import fragmentShader from '/fragment.glsl'
+//console.log(fragmentShader)
 
 
 const scene = new THREE.Scene()
@@ -14,17 +17,28 @@ const camera = new THREE.
     1000
 )
 
-const renderer = new THREE.WebGLRenderer(
-  )
+const renderer = new THREE.WebGLRenderer({
+  antialias:true
+})
 renderer.setSize(innerWidth,innerHeight)
+renderer.setPixelRatio (window.devicePixelRatio)
 document.body.appendChild (renderer.domElement)
 
 //create a sphere
 const sphere = new THREE.Mesh(
   new THREE.SphereGeometry(5, 50, 50),
-  new THREE.MeshBasicMaterial({
-    color: 0xff0000
-    //map : new THREE.TextureLoader().load()
+  //new THREE.MeshBasicMaterial({
+  new THREE.ShaderMaterial({
+    //color: 0xff0000,
+    vertexShader,
+    fragmentShader,
+    uniforms:{
+      globeTexture:{
+        value: new TextureLoader().load('/globe.jpg')
+
+      }
+    }
+   //map : new THREE.TextureLoader().load('globe.jpg')
   })
 )
 
