15b205b9f7635c4e3473c732fd1f808971da1959
prasadikduaclk <115408126+prasadikduaclk@users.noreply.github.com>
2022-10-24 15:32:27 +0530

    3 files modified

--- vite.config.js
+++ vite.config.js
@@ -3,3 +3,10 @@ import vitePluginString from 'vite-plugin-string'
 export default {
   plugins: [vitePluginString()]
 }
+
+import glsl from 'vite-plugin-glsl';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [glsl()]
+});
